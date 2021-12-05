6b2be5e15fa02eca60d14415d7b6fd33c0842ed7
aaron.barlow <aaron.barlow@nps.edu>
2021-12-05 08:34:36 -0800

    Added WBTC

--- scripts/incentives.py
+++ scripts/incentives.py
@@ -7,6 +7,7 @@ import time
 assets = { 'Aave Avalanche Market USDC' : "0x46a51127c3ce23fb7ab1de06226147f446e4a857",
            'Aave Avalanche Market variable debt vUSDC' : '0x848c080d2700cbe1b894a3374ad5e887e5ccb89c',
            'Aave Avalanche Market DAI' : '0x47afa96cdc9fab46904a55a6ad4bf6660b53c38a',
+           'Aave Avalanche Market wBTC.e' : '0x686bef2417b6dc32c50a3cbfbcc3bb60e1e9a15d',
            'Aave Avalanche Market variable debt vDAI' : '0x1852dc24d1a8956a0b356aa18ede954c7a0ca5ae',
            'Aave Avalanche Market variable debt vWAVAX ' : '0x66a0fe52fb629a6cb4d10b8580afdffe888f5fd4',
            'Aave Avalanche Market WAVAX' : '0xdfe521292ece2a4f44242efbcd66bc594ca9714b'}
